c05ae2b6c2145bcf7e7d14ce95053f49246ef601
mateusz <siiikor.sikorski@gmail.com>
2023-12-05 13:26:13 +0100

    Added simple solution to 7_reverse_integer

---
+++ 7_reverse_integer/src/Solution.java
@@ -0,0 +1,34 @@
+public class Solution {
+
+    public static int reverse(int x) {
+
+        String value = String.valueOf(x);
+
+        var output = new StringBuilder();
+
+        boolean isNegative = value.charAt(0) == '-';
+        int endIndex = 0;
+        if (isNegative) {
+            output.append('-');
+            endIndex = 1;
+        }
+
+        for (int i = value.length() - 1; i >= endIndex; i--) {
+            output.append(value.charAt(i));
+        }
+
+        try {
+            return Integer.parseInt(output.toString());
+        } catch ( NumberFormatException ex) {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        var x = -1111111118;
+        var output = reverse(x);
+
+        System.out.println(output);
+    }
+}
